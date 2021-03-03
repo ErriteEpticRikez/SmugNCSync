b8a10e36f4280d621189eaec19a77d45c55fbfd5
Michael Riley <riley117@pnw.edu>
2021-03-03 10:05:49 -0600

    Added Test Code

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,8 @@ FROM ubuntu:focal
 FROM golang:1.16.0-buster
 
 ADD main.py /
-ADD configurator.py /
 ADD errite/ /errite/
 ADD config.json /
 RUN pip3 install paramiko
-RUN apt install git openssh-server
+RUN apt install git openssh-server nano
 CMD [ "python3", "./monitorSystem.py" ]
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import paramiko
 import json
+import os
+import subprocess
 
 running = True
 dead = False
@@ -14,8 +16,24 @@ with open("config.json", "r") as configJson:
     smugdl_home = configData["smug-home"]
     setup = configData["setup"]
 
-while running:
-    ssh = paramiko.SSHClient()
-    ssh.connect(server, username=username, password=password)
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
-    ssh.close()
+print("Runniung check conditions")
+passedChecks = False
+if os.path.exists(smugdl_home):
+    print("Smugmug DL Home exists")
+    if os.path.exists(os.path.join(smugdl_home, "config.toml")):
+        print("Smugmug Downloader config present")
+        if os.path.exists(os.path.join(smugdl_home, "/cmd/smugmug-backup/main")):
+            print("Main go file for SmugMug downloader is present.")
+            passedChecks = True
+
+
+if passedChecks:
+    if setup:
+        while running:
+            subprocess.run(smugdl_home + "/cmd/smugmug-backup/main")
+            ssh = paramiko.SSHClient()
+            ssh.connect(server, username=username, password=password)
+            ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("Wall it worked")
+            ssh.close()
+    else:
+        print("Your configuration has been ")
